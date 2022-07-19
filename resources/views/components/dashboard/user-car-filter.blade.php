b65038a4c90151f98cdb9cfdfad6e4ba78477ef1
Ilxomjon Abdullayev <abdullayevilxomjon1@gmail.com>
2022-07-19 13:09:02 +0500

    user cars filter

--- resources/views/components/dashboard/user-car-filter.blade.php
+++ resources/views/components/dashboard/user-car-filter.blade.php
@@ -2,7 +2,10 @@
     <form action="">
         <x-dashboard.form-select label="Model" name="model_id" option="name" :options="$models" />
         <x-dashboard.form-select label="User" name="user_id" option="full_name" :options="$users" />
-        <button class="btn btn-primary w-100">Filter</button>
+        <div class="d-flex">
+            <a class="btn btn-warning mr-auto" href="{{ url()->current() }}">Cancel</a>
+            <button class="btn btn-primary">Filter</button>
+        </div>
     </form>
 
 </div>
