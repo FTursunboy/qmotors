88366fa95d49d20c32e904d9777ccd05a7c06a03
Ilxomjon <abdullayevilxomjon1@gmail.com>
2022-08-31 12:27:13 +0500

    review order required

--- app/Http/Requests/Review/StoreReviewRequest.php
+++ app/Http/Requests/Review/StoreReviewRequest.php
@@ -24,7 +24,7 @@ class StoreReviewRequest extends FormRequest
     public function rules()
     {
         return [
-            'order_id' => 'exists:orders,id',
+            'order_id' => 'required|exists:orders,id',
             'rating' => 'required|numeric'
         ];
     }
