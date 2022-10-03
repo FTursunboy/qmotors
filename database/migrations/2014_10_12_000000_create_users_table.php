a90f4d5a519ee0926691d68e9d26fc108246b785
Ilxomjon <abdullayevilxomjon1@gmail.com>
2022-10-03 14:45:24 +0500

    except user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,12 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->id();
+            // $table->id();
             // $table->string('name');
             // $table->string('email')->unique();
             // $table->timestamp('email_verified_at')->nullable();
             // $table->string('password');
-            $table->rememberToken();
+            // $table->rememberToken();
             $table->integer('sms_code')->nullable();
             // $table->timestamps();
         });
