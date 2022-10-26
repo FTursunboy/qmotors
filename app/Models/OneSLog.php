975010c5659ea6748ed725f727687cede17e6cf4
root <root@659501-cn80324.tmweb.ru>
2022-10-26 13:17:32 +0300

    from server

---
+++ app/Models/OneSLog.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OneSLog extends Model
+{
+    use HasFactory;
+}
