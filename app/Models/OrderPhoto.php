c2a1a14fa4bb7a26ae175dbabec8109f6261a784
Ilxomjon <abdullayevilxomjon1@gmail.com>
2022-11-10 08:10:29 +0500

    sad

--- app/Models/OrderPhoto.php
+++ app/Models/OrderPhoto.php
@@ -10,5 +10,11 @@ class OrderPhoto extends Model
 {
     use ModelCommonMethods;
     use HasFactory;
+
     protected $guarded = [];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
 }
