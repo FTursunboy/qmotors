9605102ac5b4fda3e39c6e4f7030ba11b7944ebb
Ilxomjon <abdullayevilxomjon1@gmail.com>
2022-11-15 14:57:33 +0500

    order details

--- resources/views/components/dashboard/order-table.blade.php
+++ resources/views/components/dashboard/order-table.blade.php
@@ -1,5 +1,5 @@
 <div class="d-flex mb-2">
-    <x-dashboard.table-search-form />
+    <x-dashboard.table-search-form/>
     <a href="{{ route('order.create') }}" class="ml-auto btn btn-primary">Добавить</a>
 </div>
 <x-dashboard.default-table length="{{ count($list) }}">
@@ -26,6 +26,9 @@
             <x-column-order-caret column="order_type_id">Тип Заказа</x-column-order-caret>
         </th>
         <th>
+            <x-column-order-caret column="order_status">Статус</x-column-order-caret>
+        </th>
+        <th>
             <x-column-order-caret column="guarantee">Запрос по гарантии</x-column-order-caret>
         </th>
         <th>
@@ -44,34 +47,35 @@
     </x-slot>
     <x-slot name="body">
         @foreach ($list as $index => $item)
-        <tr>
-            <td><a href="{{ route('order.show', $item->id) }}">{{ $item->id }}</a></td>
-            <td>{{ $item->order_number }}</td>
-            <td> <a href="{{ route('user-car.show', $item->user_car_id) }}">{{ $item->user_car->title }}</a></td>
-            <td>{{ $item->mileage }}</td>
-            <td>{{ optional($item->tech_center)->title }}</td>
-            <td>{{ $item->date }}</td>
-            <td>{{ optional($item->order_type_relation)->name }}</td>
-            <td>{{ $item->guarantee_text }}</td>
-            <td>{{ $item->guarantee_text }}</td>
-            <td>{{ $item->free_diagnostics_text }}</td>
-            <td>{{ $item->stock_text }}</td>
-            <td>{{ localDatetime($item->created_at) }}</td>
-            <td>
-                <div class="d-flex float-right">
-                    <a class="btn btn-info mr-2" href="{{ route('order.show', $item->id) }}"><i
-                            class="fa fa-eye"></i></a>
-                    <a href="{{ route('order.edit', $item->id) }}" class="btn btn-primary mr-2"><i
-                            class="fa fa-pen"></i></a>
-                    <form action="{{ route('order.delete', $item->id) }}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" onclick="return confirm('Ты уверен?')" class="btn btn-danger"><i
-                                class="fa fa-trash"></i></button>
-                    </form>
-                </div>
-            </td>
-        </tr>
+            <tr>
+                <td><a href="{{ route('order.show', $item->id) }}">{{ $item->id }}</a></td>
+                <td>{{ $item->order_number }}</td>
+                <td><a href="{{ route('user-car.show', $item->user_car_id) }}">{{ $item->user_car->title }}</a></td>
+                <td>{{ $item->mileage }}</td>
+                <td>{{ optional($item->tech_center)->title }}</td>
+                <td>{{ $item->date }}</td>
+                <td>{{ optional($item->order_type_relation)->name }}</td>
+                <td>{{ $item->order_status }}</td>
+                <td>{{ $item->guarantee_text }}</td>
+                <td>{{ $item->guarantee_text }}</td>
+                <td>{{ $item->free_diagnostics_text }}</td>
+                <td>{{ $item->stock_text }}</td>
+                <td>{{ localDatetime($item->created_at) }}</td>
+                <td>
+                    <div class="d-flex float-right">
+                        <a class="btn btn-info mr-2" href="{{ route('order.show', $item->id) }}"><i
+                                class="fa fa-eye"></i></a>
+                        <a href="{{ route('order.edit', $item->id) }}" class="btn btn-primary mr-2"><i
+                                class="fa fa-pen"></i></a>
+                        <form action="{{ route('order.delete', $item->id) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" onclick="return confirm('Ты уверен?')" class="btn btn-danger"><i
+                                    class="fa fa-trash"></i></button>
+                        </form>
+                    </div>
+                </td>
+            </tr>
         @endforeach
     </x-slot>
 </x-dashboard.default-table>
