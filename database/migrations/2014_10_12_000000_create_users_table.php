812019740c32b829191fe1acd85d0ebb4ad45a05
Tursunboy Faiziev <107097209+FTursunboy@users.noreply.github.com>
2024-01-16 15:19:47 +0500

    initial commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,15 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
-            // $table->id();
-            // $table->string('name');
-            // $table->string('email')->unique();
-            // $table->timestamp('email_verified_at')->nullable();
-            // $table->string('password');
-            // $table->rememberToken();
+        Schema::create('users', function (Blueprint $table) {
+             $table->id();
+            $table->string('name');
+             $table->string('email')->unique();
+             $table->timestamp('email_verified_at')->nullable();
+             $table->string('password');
+             $table->rememberToken();
             $table->integer('sms_code')->nullable();
-            // $table->timestamps();
+             $table->timestamps();
         });
     }
 
